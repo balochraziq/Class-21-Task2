3699fd639c085f83abd4768a4170f240e3ff8de9
Raziq Ali <139381746+balochraziq@users.noreply.github.com>
2023-11-15 20:50:27 -0800

    Class#21-Task2

---
+++ app.js
@@ -0,0 +1,29 @@
+function Myfuncty() {
+    let objet = document.getElementById('Mytext')
+    if (objet.type === 'password') {
+        objet.type = "text"
+    } else {
+        objet.type = "password"
+    }
+}
+
+
+
+
+
+let inputEl = document.getElementById('Myinput') 
+    inputEl.style.width= '11.5rem';
+    inputEl.style.height= '1.6rem'
+    inputEl.style.backgroundColor='darkseagreen';
+    inputEl.style.Color='#f4f6ff';
+    inputEl.style.borderRadius='0.3rem';
+
+let input = document.getElementById('Mytext') 
+    input.style.bordeRadius='0.3rem';
+    input.style.width= '11.5rem';
+    input.style.backgroundColor='darkseagreen';
+    input.style.height= '1.6rem'
+    input.style.Color='#f4f6ff';
+
+
+
